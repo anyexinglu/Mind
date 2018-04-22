79b79c072178f2eeea23d213c894ac2d1666f047
yangxiayan <yangxiayan@baidu.com>
2018-04-22 10:21:43 +0800

    Initial Commit

---
+++ Mind/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mind
+//
+//  Created by Yang,Xiayan on 2018/4/22.
+//  Copyright © 2018年 Yang,Xiayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
