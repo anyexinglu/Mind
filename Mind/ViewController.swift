43db4b952576b804b71c03996ea3797d26434b23
yangxiayan <yangxiayan@baidu.com>
2018-04-22 20:43:31 +0800

    Init the repos

--- Mind/ViewController.swift
+++ Mind/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var mainScrollView: UIScrollView!
+
+    var imageArray = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mainScrollView.frame = view.frame
+
+        imageArray = [#imageLiteral(resourceName: "WechatIMG1680.jpeg"), #imageLiteral(resourceName: "WechatIMG1681.jpeg"), #imageLiteral(resourceName: "1.png"), #imageLiteral(resourceName: "2.png")]
+
+        for i in 0..<imageArray.count{
+            let imageView = UIImageView()
+            imageView.image = imageArray[i]
+            // contentMode用来设置图片的显示方式，如居中、居右，是否缩放
+            // UIViewContentModeScaleAspectFit会保证图片比例不变，且全部显示在ImageView中，则ImageView会有部分空白。
+            imageView.contentMode = .scaleAspectFit
+            let xPosition = self.view.frame.width * CGFloat(i)
+            imageView.frame = CGRect(x: xPosition, y: 0, width: self.mainScrollView.frame.width, height: self.mainScrollView.frame.height)
+            mainScrollView.contentSize.width = mainScrollView.frame.width * CGFloat(i + 1)
+            mainScrollView.addSubview(imageView)
+        }
     }
 
     override func didReceiveMemoryWarning() {
