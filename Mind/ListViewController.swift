62a28a56f59a3074aa7d15d7a0813dfa3a7d2c5c
yangxiayan <yangxiayan@baidu.com>
2018-05-01 23:56:05 +0800

    Import UITableView

--- Mind/ListViewController.swift
+++ Mind/ListViewController.swift
@@ -8,16 +8,34 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var firstTableView: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = "我是第 \(indexPath.row) 个Cell"
+        
+        return cell
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        firstTableView.delegate = self
+        firstTableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func ToHome(_ sender: UIButton) {
-        print("toHome", self, self.view, self.view.window)
         self.view.window?.rootViewController?.dismiss(animated: true, completion: nil)
     }
     override func didReceiveMemoryWarning() {
